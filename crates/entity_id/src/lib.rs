d2dca07491e987aa7df91a3f2846b4f29c491c62
Marshall Bowers <elliott.codes@gmail.com>
2023-03-26 13:49:55 -0400

    Add note about not depending on `__private`

--- crates/entity_id/src/lib.rs
+++ crates/entity_id/src/lib.rs
@@ -5,6 +5,9 @@ pub use entity_id_core::EntityId;
 #[cfg(feature = "derive")]
 pub use entity_id_derive::EntityId;
 
+/// Private internals needed to support the [`EntityId`](entity_id_derive::EntityId) derive macro.
+///
+/// **Do not** depend on these directly, or your code is likely to break.
 #[doc(hidden)]
 #[cfg(feature = "derive")]
 pub mod __private {
