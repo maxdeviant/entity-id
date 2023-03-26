ca884085c6812c39ef4f62a9e51fdc6e295f0d40
Marshall Bowers <elliott.codes@gmail.com>
2023-03-25 21:36:14 -0400

    Generate conversion trait implementations

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,8 @@
-use std::fmt::Display;
-
 use proc_macro::TokenStream;
 use quote::quote;
 use syn::{self, parse_macro_input, DeriveInput, Token};
-use ulid::Ulid;
-use uuid::Uuid;
 
 use symbols::*;
-
-fn unprefix_id(value: &str) -> &str {
-    value.split('_').last().to_owned().unwrap_or(value)
-}
 
 mod symbols {
     use std::fmt::{self, Display};
@@ -120,115 +112,61 @@ pub fn entity_id(input: TokenStream) -> TokenStream {
                 write!(f, "{}_{}", #prefix, self.0.to_string().to_lowercase())
             }
         }
+
+        impl From<Uuid> for #name {
+            fn from(value: Uuid) -> Self {
+                Self(Ulid::from(value))
+            }
+        }
+
+        impl From<#name> for Uuid {
+            fn from(value: #name) -> Self {
+                Self::from(value.0)
+            }
+        }
+
+        impl FromStr for #name {
+            type Err = ulid::DecodeError;
+
+            fn from_str(s: &str) -> Result<Self, Self::Err> {
+                fn unprefix_id(value: &str) -> &str {
+                    value.split('_').last().to_owned().unwrap_or(value)
+                }
+
+                let value = unprefix_id(&s);
+
+                Ok(Self(Ulid::from_string(value)?))
+            }
+        }
+
+        impl TryFrom<String> for #name {
+            type Error = ulid::DecodeError;
+
+            fn try_from(value: String) -> Result<Self, Self::Error> {
+                fn unprefix_id(value: &str) -> &str {
+                    value.split('_').last().to_owned().unwrap_or(value)
+                }
+
+                let value = unprefix_id(&value);
+
+                Ok(Self(Ulid::from_string(value)?))
+            }
+        }
+
+        impl TryFrom<&str> for #name {
+            type Error = ulid::DecodeError;
+
+            fn try_from(value: &str) -> Result<Self, Self::Error> {
+                fn unprefix_id(value: &str) -> &str {
+                    value.split('_').last().to_owned().unwrap_or(value)
+                }
+
+                let value = unprefix_id(value);
+
+                Ok(Self(Ulid::from_string(value)?))
+            }
+        }
     };
 
     expanded.into()
 }
-
-// #[macro_export]
-// macro_rules! entity_id {
-//     ($entity_id:ty, $prefix:expr) => {
-//         impl $entity_id {
-//             pub fn new() -> Self {
-//                 Self(Ulid::new())
-//             }
-
-//             pub fn unprefixed(&self) -> String {
-//                 self.0.to_string().to_lowercase()
-//             }
-//         }
-
-//         impl Display for $entity_id {
-//             fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-//                 write!(f, "{}_{}", $prefix, self.0.to_string().to_lowercase())
-//             }
-//         }
-
-//         impl From<Uuid> for $entity_id {
-//             fn from(value: Uuid) -> Self {
-//                 Self(Ulid::from(value))
-//             }
-//         }
-
-//         impl From<$entity_id> for Uuid {
-//             fn from(value: $entity_id) -> Self {
-//                 Self::from(value.0)
-//             }
-//         }
-
-//         impl FromStr for $entity_id {
-//             type Err = ulid::DecodeError;
-
-//             fn from_str(s: &str) -> Result<Self, Self::Err> {
-//                 let value = crate::unprefix_id(&s);
-
-//                 Ok(Self(Ulid::from_string(value)?))
-//             }
-//         }
-
-//         impl TryFrom<String> for $entity_id {
-//             type Error = ulid::DecodeError;
-
-//             fn try_from(value: String) -> Result<Self, Self::Error> {
-//                 let value = crate::unprefix_id(&value);
-
-//                 Ok(Self(Ulid::from_string(value)?))
-//             }
-//         }
-
-//         impl TryFrom<&str> for $entity_id {
-//             type Error = ulid::DecodeError;
-
-//             fn try_from(value: &str) -> Result<Self, Self::Error> {
-//                 let value = crate::unprefix_id(value);
-
-//                 Ok(Self(Ulid::from_string(value)?))
-//             }
-//         }
-//     };
-// }
-
-// #[cfg(test)]
-// mod tests {
-//     use std::str::FromStr;
-
-//     use super::*;
-
-//     // #[derive(EntityId)]
-//     struct UserId(Ulid);
-
-//     #[test]
-//     fn new_generates_an_id_with_the_given_prefix() {
-//         let user_id = UserId::new();
-
-//         assert!(user_id.to_string().starts_with("user_"));
-//     }
-
-//     #[test]
-//     fn unprefixed_returns_the_id_without_the_prefix() {
-//         let user_id = UserId::new();
-
-//         assert_eq!(user_id.unprefixed(), user_id.0.to_string().to_lowercase());
-//     }
-
-//     #[test]
-//     fn entity_id_from_uuid() {
-//         let uuid = Uuid::from_str("14a20d59-4d68-4bdf-aac6-8e1af037d183").unwrap();
-
-//         let user_id = UserId::from(uuid);
-
-//         assert_eq!(user_id.to_string(), "user_0mm86njkb89fftnhme3br3fmc3");
-//     }
-
-//     #[test]
-//     fn uuid_from_entity_id() {
-//         let user_id = UserId::from_str("user_2wdncp35529bet2md0kzxrj0bs").unwrap();
-
-//         let uuid = Uuid::from(user_id);
-
-//         assert_eq!(
-//             uuid,
-//             Uuid::from_str("5c6d5961-94a2-4add-a151-a09ffb890179").unwrap()
-//         )
-//     }
-// }
--- tests/integration_tests.rs
+++ tests/integration_tests.rs
@@ -1,5 +1,9 @@
-use entity_id::{self, EntityId};
+use std::str::FromStr;
+
 use ulid::Ulid;
+use uuid::Uuid;
+
+use entity_id::EntityId;
 
 #[derive(EntityId, Debug)]
 #[entity_id(prefix = "user")]
@@ -17,4 +21,25 @@ fn unprefixed_returns_the_id_without_the_prefix() {
     let user_id = UserId::new();
 
     assert_eq!(user_id.unprefixed(), user_id.0.to_string().to_lowercase());
+}
+
+#[test]
+fn entity_id_from_uuid() {
+    let uuid = Uuid::try_from("14a20d59-4d68-4bdf-aac6-8e1af037d183").unwrap();
+
+    let user_id = UserId::from(uuid);
+
+    assert_eq!(user_id.to_string(), "user_0mm86njkb89fftnhme3br3fmc3");
+}
+
+#[test]
+fn uuid_from_entity_id() {
+    let user_id = UserId::from_str("user_2wdncp35529bet2md0kzxrj0bs").unwrap();
+
+    let uuid = Uuid::from(user_id);
+
+    assert_eq!(
+        uuid,
+        Uuid::from_str("5c6d5961-94a2-4add-a151-a09ffb890179").unwrap()
+    )
 }
